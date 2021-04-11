6a7dfd5ac9b9917350c272cece70a3b69b03a225
Sárdi András <sardi.andras@gmail.com>
2021-04-11 16:44:25 +0200

    Create Zene.java

---
+++ Zene.java
@@ -0,0 +1,15 @@
+public class Zene {
+    private String eloado;
+    private String cim;
+    private int hossz;
+
+    public String getEloado() {
+        return eloado;
+    }
+
+    public String getCim() {
+        return cim;
+    }
+
+    public int getHossz() {return hossz; }
+}
